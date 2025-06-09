966193ef633fc6ab155afbe8ac3d4aded47adf14
Brendan Cooney <101305939+b-cooney@users.noreply.github.com>
2025-06-09 12:19:55 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ function App() {
       <h1 className="text-3xl font-mono mb-8">Hi, I&apos;m Brendan.</h1>
       
       <p className="font-mono mb-6">
-        I live in Boston, MA. I enjoy learning languages, music, technology, writing, traveling, geocaching and hiking with my family. I am a member of the <a href="https://abenakination.com/" className="text-blue-600 hover:underline">Abenaki tribe</a>.
+        I live outside Boston, MA. I enjoy learning languages, music, coffee, writing, traveling, geocaching and hiking with my family. I am an active member of the <a href="https://abenakination.com/" className="text-blue-600 hover:underline">Abenaki tribe</a> and focus much of my volunteer efforts there.
       </p>
       <p className="font-mono mb-6">
-        I currently work for <a href="https://www.berkshirehathaway.com/" className="text-blue-600 hover:underline">Berkshire Hathaway</a> on their Berxi platform. Previously, I worked in a number of education technology, operations and strategy roles at <a href="https://ef.edu/" className="text-blue-600 hover:underline">EF Education First</a>. In the past I served on a few boards and ran a digital transformation consultancy.
+        I currently work at <a href="https://www.berkshirehathaway.com/" className="text-blue-600 hover:underline">Berkshire Hathaway</a> on their Berxi platform. Previously, I worked in a number of leadership roles in technology, operations and strategy at <a href="https://ef.edu/" className="text-blue-600 hover:underline">EF Education First</a>. In the past I have also served on a few boards and ran a digital transformation consultancy.
       </p>
       <div className="font-mono mb-6">
-        <p>Current Projects:</p>
+        <p>Active Personal Projects:</p>
         <ul className="list-disc list-inside">
           <li>Melon Math: a math competition for K-8 students run on the <a href="https://www.base.com" className="text-blue-600 hover:underline">Base</a> platform.</li>
           <li>Native Futures: an organization dedicated to leveraging emerging technologies to benefit Native American people. Our first initiative is Pathway, an AI college/career counselor designed to help Native American students.</li>
@@ -23,9 +23,9 @@ function App() {
         <p>Want to Connect?</p>
         <ul className="list-disc list-inside">
           <li>Need to reach out directly? I&apos;m available on Signal: @bcooney.80</li>
-          <li>Connect on <a href="https://www.linkedin.com/in/cooneyb" className="text-blue-600 hover:underline">LinkedIn</a></li>
-          <li>My only <i>verified</i> social media is on <a href="https://bsky.app/profile/bcooney.info" className="text-blue-600 hover:underline">Bluesky</a></li>
-          <li>My Twitter account (used mostly for networking) is <a href="https://x.com/bcooney_info" className="text-blue-600 hover:underline">@bcooney_info</a></li>
+          <li>Connect on <a khref="https://www.linkedin.com/in/cooneyb" className="text-blue-600 hover:underline">LinkedIn</a></li>
+          <li>You can also check out my project/program management newsletter <a href="https://www.projectsunpacked.com/" className="text-blue-600 hover:underline">Projects Unpacked</a></li>
+          <li>I also keep a personal blog on <a href="https://bsky.app/profile/bcooney.info" className="text-blue-600 hover:underline">Bluesky</a></li>
         </ul>
       </div>
     </div>
